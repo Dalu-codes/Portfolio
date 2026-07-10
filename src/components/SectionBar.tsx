3022af82f22b322652d0b790ea73437754482c27
Dalu-codes <chidaluezealigo@gmail.com>
2026-07-10 22:18:04 +0100

    fourth commit

--- src/components/SectionBar.tsx
+++ src/components/SectionBar.tsx
@@ -1,14 +1,13 @@
 import { NavLinks } from "./Nav";
 
 interface SectionBarProps {
-  leftLabel: string;
+  // leftLabel: string;
   id?: string;
 }
 
 export default function SectionBar({ id }: SectionBarProps) {
   return (
     <div id={id} className="px-8 py-8 flex items-center justify-end">
-      {/* <span className="text-[13px] tracking-wide font-medium text-white/80">{leftLabel}</span> */}
       <NavLinks />
     </div>
   );
